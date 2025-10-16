fab69e681d8b26f44e33511b8ec5e449097b4870
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-10-16 23:37:48 +0000

    Update chinese.js

--- js/chinese.js
+++ js/chinese.js
@@ -7,8 +7,34 @@ const chineseNotes = [
     image: "https://example.com/images/poetry.jpg" // 示例图片URL
   },
   {
-    title: "文言文翻译技巧",
-    content: "文言文翻译需注意三点：一是理解上下文，二是掌握常见虚词（如之、乎、者、也）的用法，三是注意古今异义词。例如'之'在句中常作助词或代词，需根据语境判断。",
+    title: "html 的基本结构",
+    content: "html是一个超链接的网页语言，h5是第五代，大多数网页都是基于h5等各种语言编写的！
+
+
+<div class="code-block">
+<pre><code>
+&lt;!DOCTYPE html&gt;
+    告诉游览器用html5的方式解析此网页的html代码
+&lt;html lang="en"&gt;
+    告诉浏览器该网页是英文编写的，也可用中文编写，浏览器和辅助设备会根据这个辅助处理排版
+&lt;head&gt;
+    head--相当于开始内容,head标签里面包含的是网页目录索引，相当于书的封皮，head /head为称之为双标签，就是成对出现不可单独使用，
+    &lt;br&gt;--表示换行,单独出现,名单标签<br>
+    &lt;meta charset="UTF-8"&gt;
+    &lt;meta&gt;：--是 HTML 中的元数据标签，用于提供关于网页的额外信息
+    charset="UTF-8"：--指定网页使用的字符编码方式为 UTF-8
+    为什么需要这个标签？--"如果网页里有中文，但没有这句代码，将会导致中文乱码。<br>
+    "UTF-8 --是目前最常用的字符集编码方式，它包含全世界所有国家需要用到的字符（包括中文、英文、数字、符号等）。
+    &lt;title&gt;标题标签&lt;/title&gt;
+&lt;/head&gt;
+&lt;body&gt;  body包含的是网页的内容部分
+    &lt;p&gt;这是代码示例内容&lt;/p&gt;
+&lt;/body&gt;
+
+&lt;/html&gt;  至此代码结束
+
+</code></pre>
+</div>。",
     timestamp: "2025-10-16 14:20",
     embed: '<iframe src="//player.bilibili.com/player.html?isOutside=true&aid=115270177791971&bvid=BV1BBngzfEws&cid=32649840988&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>'
   },
@@ -20,6 +46,7 @@ const chineseNotes = [
     embed: '<iframe src="//player.bilibili.com/player.html?isOutside=true&aid=115270177791971&bvid=BV1BBngzfEws&cid=32649840988&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>'
   }
 ];
+
 
 
 
