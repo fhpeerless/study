64e907438a7d33ea1859b80b8d1af03c8181062b
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-10-16 23:25:55 +0000

    Update chinese.js

--- js/chinese.js
+++ js/chinese.js
@@ -2,7 +2,7 @@
 const chineseNotes = [
   {
     title: "初识html可视化编程！",
-    content: "<br>html可视化编程可以下载然后格式改成html即可！",
+    content: "<br>html可视化编程可以下载然后格式改成html即可<a href="https://www.jetbrains.com/webstorm/">webstorm</a>  <br>或者更简单的方式实现可视化， <br>新建一个txt文本把h5代码复制进去，<br>！",
     timestamp: "2025-10-17 08:30",
     image: "https://example.com/images/poetry.jpg" // 示例图片URL
   },
@@ -20,6 +20,7 @@ const chineseNotes = [
     embed: '<iframe src="//player.bilibili.com/player.html?isOutside=true&aid=115270177791971&bvid=BV1BBngzfEws&cid=32649840988&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>'
   }
 ];
+
 
 
 
