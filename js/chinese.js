71dfcce959678593f7b0714013fb0b280c43f4c9
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-10-17 01:16:12 +0000

    Update chinese.js

--- js/chinese.js
+++ js/chinese.js
@@ -3,7 +3,8 @@ const chineseNotes = [
   {
     title: "初识html可视化编程！",
     content: "html可视化编程可以下载<a href=\"https://www.jetbrains.com/webstorm/\">WebStorm</a>，或者更简单的方式实现可视化：新建一个txt文本把h5代码复制进去，用浏览器打开即可！",
-    timestamp: "2025-10-10 08:30"
+    timestamp: "2025-10-10 08:30",
+    image: "https://picsum.photos/seed/love1/400/300"
   },
   {
     title: "html 的基本结构",
@@ -34,6 +35,7 @@ const chineseNotes = [
              "</code></pre>",
 
     timestamp: "2025-10-11 14:20",
+    image: "https://picsum.photos/seed/love1/400/300",
     embed: '<iframe src="//player.bilibili.com/player.html?isOutside=true&aid=1251098300&bvid=BV1iJ4m1e7SA&cid=1454387685&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>'
   },
   {
@@ -42,6 +44,8 @@ const chineseNotes = [
              " &lt;u&gt; <u>下划线标签内容放在标签里面</u>  &lt;u&gt;。<br>" +
              "&lt;strike&gt;   <strike>删除线标签放在标签里面</strike>  &lt;/strike&gt; ",
     timestamp: "2025-10-12 10:00",
+    image: "https://picsum.photos/seed/love1/400/300",
     embed: '<iframe src="//player.bilibili.com/player.html?isOutside=true&aid=1251098300&bvid=BV1iJ4m1e7SA&cid=1454387685&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>'
   }
 ];
+
