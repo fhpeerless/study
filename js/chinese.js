50bdc350e24b903d3bfe968cbbd01ba4c777c394
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-10-17 00:46:29 +0000

    Update chinese.js

--- js/chinese.js
+++ js/chinese.js
@@ -6,21 +6,19 @@ const chineseNotes = [
     timestamp: "2025-10-10 08:30",
     image: "https://picsum.photos/seed/love1/400/300"
   },
-
-  
   {
     title: "html 的基本结构",
     content: "html是一个超链接的网页语言，h5是第五代，大多数网页都是基于h5等各种语言编写的！<br><br>" +
              "<div class=\"code-block\">" +
              "<pre><code>" +
              "&lt;!DOCTYPE html&gt;<br>" +
-             "    告诉游览器用html5的方式解析此网页的html代码<br>" +
+             "    告诉浏览器用html5的方式解析此网页的html代码<br>" +
              "&lt;html lang=\"en\"&gt;<br>" +
              "    告诉浏览器该网页是英文编写的，也可用中文编写<br>" +
              "    浏览器和辅助设备会根据这个辅助处理排版<br>" +
              "&lt;head&gt;<br>" +
              "    head--相当于开始内容，head标签里面包含的是网页目录索引<br>" +
-             "    相当于书的封皮，head /head为称之为双标签<br>" +
+             "    相当于书的封皮，head 和 /head 为称之为双标签<br>" +
              "    成对出现不可单独使用<br>" +
              "    &lt;br&gt;--表示换行，单独出现，单标签<br>" +
              "    &lt;meta charset=\"UTF-8\"&gt;<br>" +
@@ -40,23 +38,13 @@ const chineseNotes = [
     timestamp: "2025-10-11 14:20",
     embed: '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=2147533175&auto=1&height=66"></iframe>'
   },
-
-  
   {
     title: "html 的字体标签",
-    content: 
-      " <em> &lt;em&gt; 斜体的内容放在两个标签里面 &lt;/em&gt;斜体标签 </em>  <br>" +
-      " <strike>  &lt; strike &gt;删除线标签内容放在标签里面&lt;/strike&gt; 删除线标签</strike>。<br>" +
-      " <u> &lt;u&gt; 下划线标签放在标签里面&lt;/u&gt; 下划线标签 </u>>  <br>",
+    content: "<em>&lt;em&gt;斜体的内容放在两个标签里面&lt;/em&gt;斜体标签</em><br>" +
+             "<span style=\"text-decoration: line-through;\">&lt;del&gt;删除线标签内容放在标签里面&lt;/del&gt;删除线标签</span>。<br>" +
+             "<u>&lt;u&gt;下划线标签放在标签里面&lt;/u&gt;下划线标签</u>",
     timestamp: "2025-10-12 10:00",
     image: "https://picsum.photos/seed/love1/400/300",
     embed: '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=2147533175&auto=1&height=66"></iframe>'
   }
 ];
-
-
-
-
-
-
-
