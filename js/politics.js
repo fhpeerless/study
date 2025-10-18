4e1b13b76d4ed0e58e7fa724f8cef6ebe410c6bf
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-10-18 19:27:30 +0800

    Update politics.js

--- js/politics.js
+++ js/politics.js
@@ -7,11 +7,12 @@ const politicsNotes = [
 "毛泽东思想和中国特色社会主义理论体系概论（毛中特）: 被认为是重中之重，在考试中占比也很大。<br> " +
 "中国近现代史纲要（史纲）: 考察中国近现代历史的变迁和发展。  <br>" +
 "思想道德修养与法律基础（思修）: 考察思想道德和法律方面的知识。 <br>" +
-"形势与政策以及当代世界经济与政治（形势与政策/当代）: 考察当前的国内外形势和政策！--><a href=\"httpsbaidu.com/\">badu</a><----",
+"形势与政策以及当代世界经济与政治（形势与政策/当代）: 考察当前的国内外形势和政策！<br>--><a href=\"https://baidu.com/\">badu</a><----",
     timestamp: "2025-10-10 08:30",
     embed: '<iframe src="//player.bilibili.com/player.html?isOutside=true&aid=503996937&bvid=BV18g411u7Uj&cid=463360404&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>'
   }
 ];
+
 
 
 
