1630f7944322d81352323120551bc8bb708ebb40
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-10-18 10:18:48 +0000

    Update python.js

--- js/python.js
+++ js/python.js
@@ -2,9 +2,10 @@
 const pythonNotes = [
   {
     title: "python的安装",
-    content: "python<a href=\"https://www.python.org/\">python</a>，！",
+    content: "python官网地址<a href=\"https://www.python.org/\">python</a>，！",
     timestamp: "2025-10-10 08:30",
     image: "https://picsum.photos/seed/love1/400/300",
     embed: '<iframe src="//player.bilibili.com/player.html?isOutside=true&aid=115341732611285&bvid=BV1FSx6zaETF&cid=32931515079&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>'
   }
 ];
+
