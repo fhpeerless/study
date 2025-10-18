04deb17bee3f39d2e37c089480a9c476c8451cd5
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-10-18 11:00:25 +0000

    Update politics.js

--- js/politics.js
+++ js/politics.js
@@ -1,12 +1,19 @@
 // 语文笔记数据
 const politicsNotes = [
   {
-    title: "初识html可视化编程！",
-    content: "html可视化编程可以下载<a href=\"https://www.jetbrains.com/webstorm/\">WebStorm</a>，或者更简单的方式实现可视化：新建一个txt文本把h5代码复制进去，用浏览器打开即可！",
+    title: "主要考查科目",
+    content: "
+"马克思主义基本原理概论（马原）: 考试中占有重要比重，通常是难点和易失分点。 " +
+"毛泽东思想和中国特色社会主义理论体系概论（毛中特）: 被认为是重中之重，在考试中占比也很大。 " +
+"中国近现代史纲要（史纲）: 考察中国近现代历史的变迁和发展。  " +
+"思想道德修养与法律基础（思修）: 考察思想道德和法律方面的知识。 " +
+"形势与政策以及当代世界经济与政治（形势与政策/当代）: 考察当前的国内外形势和政策！<a href=\"https://www.jetbrains.com/webstorm/\">WebStorm</a>，或者更简单的方式实现可视化：新建一个txt文本把h5代码复制进去，用浏览器打开即可！",
     timestamp: "2025-10-10 08:30",
     image: "https://picsum.photos/seed/love1/400/300"
+    <iframe src="//player.bilibili.com/player.html?isOutside=true&aid=503996937&bvid=BV18g411u7Uj&cid=463360404&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
   }
 ];
+
 
 
 
