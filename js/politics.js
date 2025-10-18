d5070dcfeebf5129aca59da22ddda5a80d1f3c91
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-10-18 10:55:05 +0000

    Update politics.js

--- js/politics.js
+++ js/politics.js
@@ -1,5 +1,5 @@
 // 语文笔记数据
-const physicsNotes = [
+const politicsNotes = [
   {
     title: "初识html可视化编程！",
     content: "html可视化编程可以下载<a href=\"https://www.jetbrains.com/webstorm/\">WebStorm</a>，或者更简单的方式实现可视化：新建一个txt文本把h5代码复制进去，用浏览器打开即可！",
@@ -7,5 +7,6 @@ const physicsNotes = [
     image: "https://picsum.photos/seed/love1/400/300"
   }
 ];
+
 
 
