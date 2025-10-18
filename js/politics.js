d95ab533226b6fb05585785a7d664afe4c9f094c
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-10-18 19:09:54 +0800

    Update politics.js

--- js/politics.js
+++ js/politics.js
@@ -2,7 +2,7 @@
 const politicsNotes = [
   {
     title: "主要考查科目",
-    content: "
+    content: 
 "马克思主义基本原理概论（马原）: 考试中占有重要比重，通常是难点和易失分点。 " +
 "毛泽东思想和中国特色社会主义理论体系概论（毛中特）: 被认为是重中之重，在考试中占比也很大。 " +
 "中国近现代史纲要（史纲）: 考察中国近现代历史的变迁和发展。  " +
@@ -12,6 +12,7 @@ const politicsNotes = [
     embed: '<iframe src="//player.bilibili.com/player.html?isOutside=true&aid=503996937&bvid=BV18g411u7Uj&cid=463360404&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>'
   }
 ];
+
 
 
 
