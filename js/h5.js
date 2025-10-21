fc87e0da15adbdff046beca0b42575fcdbb1c74f
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-10-21 13:24:12 +0800

    Update h5.js

--- js/h5.js
+++ js/h5.js
@@ -50,13 +50,14 @@ const h5Notes = [
     {
     title: "文字的上标和下标标签！",
     content: "x<sup>2</sup> 写法为：x&lt;sup&gt;2&lt;/sup&gt;   <br>  x<sub>2</sub> 写法为：x&lt;sub&gt;2&lt;/sub&gt;",
-    timestamp: "2025-10-12 08:30",
+    timestamp: "2025-10-13 08:30",
     images: [  // 正确属性名：images（复数）
       "https://picsum.photos/seed/love1/400/300",
       "https://picsum.photos/seed/love3/400/300"
     ]
   }
 ];
+
 
 
 
