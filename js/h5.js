80f7438fe5f242fc320fc2f9147ef78fbcdf0416
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-10-23 18:43:28 +0800

    Update h5.js

--- js/h5.js
+++ js/h5.js
@@ -2,7 +2,7 @@
 const h5Notes = [
   {
     title: "初识html可视化编程！",
-    content: "html可视化编程可以下载<a href=\"https://www.jetbrains.com/webstorm/\">WebStorm</a>，或者更简单的方式实现可视化：新建一个txt文本把h5代码复制进去，用浏览器打开即可！，更多代码查询请点击--><a href=\"https://www.w3schools.com/html/html_images.asp\">W3c</a>",
+    content: "html可视化编程可以下载<a href=\"https://www.jetbrains.com/webstorm/\">WebStorm</a>，或者更简单的方式实现可视化：新建一个txt文本把h5代码复制进去，用浏览器打开即可！，<br>更多代码查询请点击--><a href=\"https://www.w3schools.com/html/html_images.asp\">W3c</a>",
     timestamp: "2025-10-10 08:30",
     images: [  
     ],
@@ -67,6 +67,7 @@ const h5Notes = [
 
   
 ];
+
 
 
 
