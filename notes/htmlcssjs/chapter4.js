e517e95b7aaeddcbad0054d87160de1f7d8b8153
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-11-04 11:03:40 +0800

    Update chapter4.js

--- notes/htmlcssjs/chapter4.js
+++ notes/htmlcssjs/chapter4.js
@@ -2,7 +2,7 @@
 export const note1 = {
     title: "css初识",
     content: [   /* 在JSON中，字符串内部的双引号必须用反斜杠转义，即\"。以下是修正后的JSON： */
-        "<h2>css概述 </h2> <hr width='15%'>" ,
+        "<h2>css概述（了解以下即可）</h2> <hr width='15%'>" ,
         "可以把html比作造型的过程，则css相当于给素描上色的过程，html编写内容，css布局等,js相当于动画编写" ,
         "点击了解--><a href=\"https://blog.csdn.net/m0_67840539/article/details/130824794\">css的发展史</a><-- " ,
         "点击了解，不同时期的css代码进化后而随之改变的网页样式--><a href=\"https://www.webdesignmuseum.org/all-websites\">css发展之网页的变迁</a><-- "
@@ -17,6 +17,7 @@ export const note1 = {
 // 统一导出笔记数组（关键：让JS加载时能获取所有笔记）, note2, note3, note4, note5, note6, note7, note8, note9
 
 export default [note1];
+
 
 
 
