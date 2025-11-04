49c7169b17151c6a3a53c36e1b7c140a9d842753
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-11-05 06:39:46 +0800

    Update chapter4.js

--- notes/htmlcssjs/chapter4.js
+++ notes/htmlcssjs/chapter4.js
@@ -14,22 +14,36 @@ export const note1 = {
 
 // 第一篇笔记
 export const note2 = {
-    title: "css的基本结构和重点属性",
+    title: "css的基本结构和属性标签",
     content: [   /* 123123*/
         "<h3>css标签参考手册</h3>--><a href=\"https://www.w3school.com.cn/cssref/index.asp\">css的参考手册</a><-- " ,
         "<h3>css属性基本使用展示</h3><img src=\"http://note.youdao.com/yws/api/personal/file/WEBd1314486bd7a0dee19b3070650ef4b21?method=download&inline=true&shareKey=3f504772f84aa261fee6b5568109a48b\" border=\"4\" style=\"max-width: 100%; height: auto;\">",
-
-        "点击了解--><a href=\"https://blog.csdn.net/m0_67840539/article/details/130824794\">css的发展史</a><-- " ,
-        "点击了解，不同时期的css代码进化后而随之改变的网页样式--><a href=\"https://www.webdesignmuseum.org/all-websites\">css发展之网页的变迁</a><-- "
+        "<h3>css的代码位置</h3> <br>css代码可打包成单独的文件后引入，或者直接写在head标签内">"
+        
     ],
     timestamp: "2025-10-18 08:30",
     images: [],  // 确保有images属性
     embed: ""
 };
 
+
+export const note3 = {
+    title: "css的基本结构和属性标签",
+    content: [   /* 123123*/
+        "<h3>css标签参考手册</h3>--><a href=\"https://www.w3school.com.cn/cssref/index.asp\">css的参考手册</a><-- " ,
+        "<h3>css属性基本使用展示</h3><img src=\"http://note.youdao.com/yws/api/personal/file/WEBd1314486bd7a0dee19b3070650ef4b21?method=download&inline=true&shareKey=3f504772f84aa261fee6b5568109a48b\" border=\"4\" style=\"max-width: 100%; height: auto;\">",
+
+    ],
+    timestamp: "2025-10-18 08:30",
+    images: [],  // 确保有images属性
+    embed: ""
+};
+
+
 // 统一导出笔记数组（关键：让JS加载时能获取所有笔记）, note2, note3, note4, note5, note6, note7, note8, note9
 
 export default [note1,note2];
+
 
 
 
