508560bbfe12348f73ae8a3c4e7d996d8e1da6af
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-11-07 00:47:09 +0000

    Update chapter1.js

--- notes/english/chapter1.js
+++ notes/english/chapter1.js
@@ -104,5 +104,6 @@ export const note12 = {
 };
 // 统一导出笔记数组（关键：让JS加载时能获取所有笔记）
 
-export default [note1, note2, note3, note4, note5, note6, note7, note8, note9, note10, note11, note12];
+// export default [note1, note2, note3, note4, note5, note6, note7, note8, note9, note10, note11, note12];
+export default [note1];
 
