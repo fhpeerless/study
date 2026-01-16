7826fef7b3e7f409dc106e3a30830a5e445cf8f0
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2026-01-16 10:59:45 +0800

    Add files via upload

--- notes/css/chapter4.js
+++ notes/css/chapter4.js
@@ -1,12 +1,20 @@
 // 第一篇笔记
 export const note1 = {
     title: "css初识",
-    content: [   /* 在JSON中，字符串内部的双引号必须用反斜杠转义，即\"。以下是修正后的JSON： */
-        "<h2>css概述（了解以下即可）</h2> <hr width='15%'>" ,
-        "可以把html比作造型的过程，则css相当于给素描上色的过程，html编写内容，css布局等,js相当于动画编写" ,
-        "点击了解--><a href=\"https://baike.baidu.com/item/CSS/5457 target=\"_blank\" \">css的发展史</a><-- " ,
-        "点击了解，不同时期的css代码进化后而随之改变的网页样式--><a href=\"https://www.webdesignmuseum.org/all-websites\">css发展之网页的变迁</a><-- "
-    ],
+    contentmd:` 
+### css概述（了解一下即可）
+
+- 可以把html比作造型的过程，则css相当于给素描上色的过程，html编写内容，css布局等,js相当于动画编写
+ ### css的发展史
+ /* 在JSON中，字符串内部的双引号必须用反斜杠转义，即\"。以下是修正后的JSON： */
+<br>   
+点击--> [css的发展史](https://www.w3school.com.cn/tags/tag_map.asp "WebStorm") 了解css的发展史
+
+点击--> [css的发展史](https://www.webdesignmuseum.org/all-websites\ "WebStorm") 了解css的发展史
+
+点击--> [css发展之网页的变迁](https://www.webdesignmuseum.org/all-websites\ "WebStorm") 了解，不同时期的css代码进化后而随之改变的网页样式-->
+
+`,
     timestamp: "2025-10-18 08:30",
     images: [],  // 确保有images属性
     embed: ""
@@ -15,12 +23,16 @@ export const note1 = {
 // 第一篇笔记
 export const note2 = {
     title: "css的基本结构和属性标签",
-    content: [   /* 123123*/
-        "<h3>css标签参考手册</h3>--><a href=\"https://www.w3school.com.cn/cssref/index.asp\">css的参考手册</a><-- " ,
-        "<h3>css属性基本使用展示</h3><img src=\"http://note.youdao.com/yws/api/personal/file/WEBd1314486bd7a0dee19b3070650ef4b21?method=download&inline=true&shareKey=3f504772f84aa261fee6b5568109a48b\" border=\"4\" style=\"max-width: 100%; height: auto;\">",
-        "<h3>css的代码位置</h3> <br>css代码可打包成单独的文件后引入，或者直接写在head标签内"
-
-    ],
+    contentmd:`
+   /* 123123*/
+## css标签参考手册
+点击--> [css标签参考手册](https://www.w3school.com.cn/cssref/index.asp "WebStorm") 了解ccss的参考手册
+## css属性基本使用展示
+点击--> [css语法使用w3c](https://www.w3school.com.cn/css/css_syntax.asp "WebStorm") 了解ccss的参考手册
+## css的代码位置
+css代码可打包成单独的文件后引入，或者直接写在head标签内
+       
+`,
     timestamp: "2025-10-18 08:30",
     images: [],  // 确保有images属性
     embed: ""
@@ -28,13 +40,24 @@ export const note2 = {
 
 export const note3 = {
     title: "属性选择器",
-    content: [   /* 123123*/
-        "<h3>css选择器功能就是选择指定的元素，进行样式布局的修改<br>css选择器的几大类</h3>--><a href=\"https://www.w3school.com.cn/css/css_selectors.asp\" target=\"_blank\">css的几类选择器</a><-- <br>" ,
+    contentmd:`
+## css选择器功能就是选择指定的元素，进行样式布局的修改
 
-        "<h3>属性选择器是选择带有指定属性的元素，官方介绍</h3>--><a href=\"https://www.w3school.com.cn/cssref/selector_attribute.asp\" target=\"_blank\">css的属性选择器</a><-- <br>" ,
-        "<h3>属性选择器的在线代码演示</h3>  <a href=\"https://www.w3school.com.cn/tiy/t.asp?f=selector_attribute\" title=\"属性在线编辑器\">点击进入属性选择器代码展示</a>"
+## css选择器的几大类
 
-    ],
+点击--> [css选择器](https://www.w3school.com.cn/css/css_selectors.asp "WebStorm") 了解css选择器
+
+## 属性选择器
+
+属性选择器是选择带有指定属性的元素，官方介绍
+
+点击--> [css属性选择器](https://www.w3school.com.cn/cssref/selector_attribute.asp "WebStorm") 了解css的属性选择器
+
+## 属性选择器在线演示
+点击--> [属性选择器的在线代码演示](https://www.w3school.com.cn/tiy/t.asp?f=selector_attribute "WebStorm") 点击进入属性选择器代码展示
+
+ 
+`,
     timestamp: "2025-10-18 08:30",
     images: [],  // 确保有images属性
     embed: ""
@@ -43,57 +66,86 @@ export const note3 = {
 // 第一篇笔记
 export const note4 = {
     title: "class选择器和id选择器",
-    content: [   /* 123123*/
-        "<h3>css选择器介绍：</h3>--><a href=\"https://www.w3school.com.cn/cssref/css_selectors.asp\" target=\"_blank\">css的选择器参考手册</a><-- <br>class选择器通常用于一块的元素，而id选择器通常用于某一个，id多和js联系密切" ,
+    contentmd:`    /* 123123*/
+## 1.css选择器介绍
+点击--> [css选择器介绍](https://www.w3school.com.cn/cssref/css_selectors.asp "WebStorm") css的选择器参考手册  
+    
+## 2.class选择器功能
+class选择器通常用于一块的元素，而id选择器通常用于某一个，id多和js联合使用
+class选择器选择带有class属性值的标签，并加入css样式，此代码找到class为nimei的值后，更改了p标签的背景颜色
 
-        "<h3>class选择器的功能</h3>class选择器选择带有class属性值的标签，并加入css样式，此代码找到class为nimei的值后，更改了p标签的背景颜色<br> &lt;P class=\"nimei\"&gt;测试属性选择器&lt;/P&gt; &lt;style&gt; .nimei{background-color: aquamarine}&lt;/style&gt; ",
-        "<h4>class选择器的介绍</h4> --><a href=\"https://www.w3school.com.cn/cssref/selector_class.asp\" target=\"_blank\">class选择器功能介绍</a><-- ",
-        "<h4>class代码在线演示</h4> --><a href=\"https://www.w3school.com.cn/tiy/t.asp?f=selector_class_1\" target=\"_blank\">class选择器在线演示</a><-- ",
+\`\`\`css copy
+<P class="nimei">测试属性选择器</P> <style> .nimei{background-color: aquamarine}</style>
+\`\`\`
+
+### 1.1.class选择器的介绍
+
+点击--> [class选择器](https://www.w3school.com.cn/cssref/selector_class.asp "WebStorm") 了解class选择器功能介绍
+### 2.2.class代码在线演示
+
+点击--> [class代码在线演示](https://www.w3school.com.cn/tiy/t.asp?f=selector_class_1 "WebStorm") 了解class选择器在线演示
 
 
-        "<h3>id选择器的功能</h3>id选择器是选择一个带有id值的一个唯一元素，来更改他的样式<br> class可能是为都多个元素设计的",
-        "<h4>id选择器的介绍</h4> --> <a href=\"https://www.w3school.com.cn/cssref/selector_id.asp\" target=\"_blank\">id选择器介绍</a><-- ",
-        "<h4>id代码在线演示</h4> --><a href=\"https://www.w3school.com.cn/tiy/t.asp?f=selector_id\" target=\"_blank\">id选择器在线演示</a><-- ",
+## 3.id选择器的功能
+id选择器是选择一个带有id值的一个唯一元素，来更改他的样式,class可能是为都多个元素设计的
 
-        "<h3>class选择器的多种使用方法</h3> <br> --><a href=\"https://www.w3school.com.cn/cssref/selector_class.asp\" target=\"_blank\">css的class选择器</a><-- ",
+## 4.id选择器的介绍
 
-    ],
+点击--> [id选择器的介绍](https://www.w3school.com.cn/cssref/selector_id.asp "WebStorm") 了解id选择器介绍
+
+## 5.id代码在线演示
+
+点击--> [id代码在线演示](https://www.w3school.com.cn/tiy/t.asp?f=selector_id "WebStorm") 了解id选择器在线演示
+
+## 6.class选择器的多种使用方法
+点击--> [class选择器的多种使用方法](https://www.w3school.com.cn/cssref/selector_class.asp "WebStorm") 了解css的class选择器
+
+
+`,
     timestamp: "2025-10-18 08:30",
     images: [],  // 确保有images属性
     embed: ""
 };
 
+
 // 第一篇笔记
 export const note5 = {
     title: "css的伪类和伪元素选择器",
-    content: [   /* 123123*/
-        `<style>
-     /* 未访问的链接 */
-        a:link {color: brown;}
-      /* 已访问的链接 */
-      a:visited {color: orangered;} 
-      /* 鼠标悬停链接 */ 
-      a:hover {color: #FF00FF;} 
-      /* 已选择的链接 */
-      a:active {color: #0000FF;}
-        </style>`,
+    contentmd:`   
 
-        "<h2>伪类选择器和伪元素选择器</h2>" +
-        "<h3>伪类选择器功能</h3>  对选择器实现可变的特殊效果，类选择器是选择到指定类元素调整样式 --><a href=\"https://www.w3school.com.cn/css/css_pseudo_classes.asp\" title=\"伪类选择器\">伪类选择器的w3c解释</a><--" ,
+## 代码实例
+ \`\`\`css copy
+ 
+< style >
+ /* 未访问的链接 */ 
+ a:link {color: #FF0000;} 
+ /* 已访问的链接 */ 
+ a:visited {color: #00FF00;}
+  /* 鼠标悬停链接 */ 
+ a:hover {color: #FF00FF;}
+   /* 已选择的链接 */ 
+ a:active {color: #0000FF;} 
 
-        "<h4>代码展示</h4>",
-        "&lt;a href=\"https://www.w3school.com.cn/css/css_pseudo_classes.asp\" title=\"伪类选择器\"&gt; 点击进入伪类选择器&lt;/a&gt;",
-        "&lt; style &gt; /* 未访问的链接 */  a:link {color: #FF0000;}  /* 已访问的链接 */   a:visited {color: #00FF00;}   /* 鼠标悬停链接 */   a:hover {color: #FF00FF;}  /* 已选择的链接 */   a:active {color: #0000FF;}   &lt;/style &gt;",
-        "<h4>代码在线运行效果：</h4>",
+</style >
+<a href="https://www.w3school.com.cn/css/css_pseudo_classes.asp" title="伪类选择器"> 点击进入伪类选择器</a>
+\`\`\`
 
-        "<a href=\"https://www.w3school.com.cn/css/css_pseudo_classes.asp\" target=\"_blank\" title=\"伪类选择器\">点击进入伪类选择器</a>",
+## css伪类定义概念
 
-        "<h3>伪元素选择器的功能</h3>对某元素其中一部分实现特殊效果，<a href=\"https://www.w3school.com.cn/css/css_pseudo_elements.asp\" title=\"伪元素选择器\"> w3c介绍的伪元素选择器</a> ",
-        "<a href=\"https://www.w3school.com.cn/tiy/t.asp?f=css_pseudo-element\" title=\"伪元素选择器\" target=\"_blank\"> 伪元素选择器在线演示</a> ",
+点击--> [伪类定义](https://www.w3school.com.cn/css/css_pseudo_classes.asp "WebStorm") 了解伪类选择器
+
+## 伪元素选择器的功能
+
+对某元素其中一部分实现特殊效果
+
+点击--> [w3c介绍的伪元素选择器](https://www.w3school.com.cn/css/css_pseudo_elements.asp "WebStorm") 了解w3c介绍的伪元素选择器
+            
+点击--> [w3c伪元素选择器在线演示](https://www.w3school.com.cn/tiy/t.asp?f=css_pseudo-element "WebStorm") 了解伪元素选择器在线演示
 
 
+{{html:../danye/css/section_4_5.html,width:100%,height:450px}}
 
-    ],
+`,
     timestamp: "2025-10-18 08:30",
     images: [],  // 确保有images属性
     embed: ""
@@ -102,17 +154,19 @@ export const note5 = {
 // 第一篇笔记
 export const note6 = {
     title: "其他css选择器",
-    content: [   /* 123123*/
-        "<h3>css的多种选择器参考：</h3>--><a href=\"https://www.w3school.com.cn/cssref/css_selectors.asp\" target=\"_blank\">css的其他选择器</a><-- <br>" ,
+    contentmd:`   /* 123123*/
+## css的多种选择器参考
 
-        "上面的w3c网址，已经包含了在线代码测试，直接在网站测试学习即可 "
+点击--> [css的其他选择器](https://www.w3school.com.cn/cssref/css_selectors.asp "WebStorm") 了解css多种选择器
+
+上面的w3c网址，已经包含了在线代码测试，直接在网站测试学习即可
        
-
-    ],
+`,
     timestamp: "2025-10-18 08:30",
     images: [],  // 确保有images属性
     embed: ""
 };
+
 
 // 统一导出笔记数组（关键：让JS加载时能获取所有笔记）, note2, note3, note4, note5, note6, note7, note8, note9
 
