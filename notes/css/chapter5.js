739278e1ffa609836514fbda0b5264864a6c340a
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2026-01-16 11:26:05 +0800

    Add files via upload

--- notes/css/chapter5.js
+++ notes/css/chapter5.js
@@ -1,37 +1,84 @@
 export const note1 = {
     title: "文本的属性",
-    content: [
-        "<h3>css文本的相关属性</h3>--><a href=\"https://www.w3school.com.cn/cssref/index.asp\" target=\"_blank\">css的参考手册</a><--",
-        "不要特意去学属性，在工作中遇到什么属性就学什么属性即可！<br>文本的color属性：--><a href=\"https://www.w3school.com.cn/cssref/pr_text_color.asp\" target=\"_blank\">文本颜色属性</a><--<br>可用rgb或英文单词，或者#xxxxxxx三种方式",
-        "text-align，w3c参考属性--><a href=\"https://www.w3school.com.cn/cssref/pr_text_text-align.asp\" target=\"_blank\">text参考属性</a><--  <br>居中，左，右三种方式"
-    ],
+    contentmd:`
+
+## css文本的相关属性
+属性不需要特意去学，写代码的时候需要则现查即可，内容会很多的
+点击--> [w3c介绍css的参考手册](https://www.w3school.com.cn/cssref/index.asp "WebStorm") 了解css的参考手册
+## 文本的color属性
+
+点击--> [w3c介绍文本颜色属性](https://www.w3school.com.cn/cssref/pr_text_color.asp "WebStorm") 了解文字颜色属性，
+以下三种方式润色
+- 可用rgb
+- 英文单词，
+- #xxxxxxx
+
+## text的align属性
+
+详细内容为文本相关位置属性，
+点击--> [w3c介绍文本align属性](https://www.w3school.com.cn/cssref/pr_text_text-align.asp "WebStorm") 了解文字颜色属性，
+
+            
+`,
     timestamp: "2025-10-18 08:30",
     images: [],
     embed: ""
 };
 
+
 export const note2 = {
     title: "背景的相关属性",
-    content: [
-        "<h3>背景的相关属性</h3>--><a href=\"https://www.w3school.com.cn/css/css_background.asp\" target=\"_blank\">css的参考手册</a><--",
-        "不要特意去学属性，遇工作中遇到什么属性就学什么属性即可！<br>背景的相关属性，(链接内左侧侧边栏下面三个都是背景相关属性)：--><a href=\"https://www.w3school.com.cn/css/css_background.asp\" target=\"_blank\">背景的相关属性</a><--",
-        "<h3>image-repeat</h3>--><a href=\"https://www.w3school.com.cn/css/css_background_repeat.asp\" target=\"_blank\">image-repeat在线测试</a><--"
-    ],
+    contentmd:`
+
+## 背景的相关属性
+点击--> [w3c介绍背景的相关属性](https://www.w3school.com.cn/css/css_background.asp "WebStorm") 了解css的背景属性
+
+背景的相关属性，(链接内左侧侧边栏下面三个都是背景相关属性)
+
+点击--> [w3c介绍背景的相关属性](https://www.w3school.com.cn/css/css_background.asp "WebStorm") 了解css的背景属性
+
+## 背景相关方式属性
+点击--> [w3c介绍image-repeat属性](https://www.w3school.com.cn/css/css_background_repeat.asp "WebStorm") 了解image-repeat在线测试
+
+    
+    
+`,
     timestamp: "2025-10-18 08:30",
     images: [],
     embed: ""
 };
 
 export const note3 = {
-    title: "列表的相关属性",
-    content: [
-        "<h3>列表的相关属性</h3>--><a href=\"https://www.w3school.com.cn/css/css_list.asp\" target=\"_blank\">w3c列表属性说明</a><--",
-        "不要特意去学属性，遇工作中遇到什么属性就学什么属性即可！<br>：--><a href=\"https://www.w3school.com.cn/css/css_background.asp\" target=\"_blank\">背景的相关属性</a><--",
-        "<h3>image-repeat</h3>--><a href=\"https://www.w3school.com.cn/css/css_background_repeat.asp\" target=\"_blank\">image-repeat在线测试</a><--",
-        "<h3>元素位置控制</h3>--><a href=\"https://www.w3school.com.cn/cssref/pr_class_position.asp\" target=\"_blank\">position</a><--",
-        "<h3>padding属性的介绍</h3>--><a href=\"https://www.w3school.com.cn/cssref/pr_padding.asp\" target=\"_blank\">paddin——w3c</a><--",
-        "<br> height和width表示的div所占的区域 <br> &lt;style&gt; .mes { width:1000px; height:10px;} &lt;/style&gt; &lt;/head&gt; &lt;body&gt; &lt;div className=\"mes\"&gt; 宝宝特卖&lt;/div&gt;"
-    ],
+title: "列表的相关属性",
+contentmd:`
+## 列表的相关属性
+点击--> [w3c介绍列表的相关属性](https://www.w3school.com.cn/css/css_list.asp "WebStorm") 了解列表属性说明
+
+## 元素位置控制
+
+点击--> [w3c介绍元素位置控制](https://www.w3school.com.cn/cssref/pr_class_position.asp "WebStorm") 了解元素位置控制
+
+## padding属性的介绍
+点击--> [w3c介绍padding属性的介绍](https://www.w3school.com.cn/cssref/pr_padding.asp "padding") 了解padding属性的介绍
+
+## 代码实例
+
+ \`\`\`css copy
+height和width表示的div所占的区域
+<head>
+<style> 
+.mes {
+ width:1000px; height:10px;} 
+</style> 
+</head> 
+<body>
+ 
+ <div className="mes"> 宝宝特卖</div>
+ 
+ </body>>
+  \`\`\`
+       
+`,
     timestamp: "2025-10-18 08:30",
     images: [],
     embed: ""
