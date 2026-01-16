d4b4e5d814cb38a8275cc3bc5dc13675a824effb
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2026-01-16 06:05:04 +0000

    Update chapter5.js

--- notes/css/chapter5.js
+++ notes/css/chapter5.js
@@ -104,7 +104,7 @@ console.log("Hello, World!");
 \`\`\`
 
 ### 图片示例
-![Markdown图片示例](https://picsum.photos/800/400 "width=400 height=400")
+![Markdown图片示例](https://note.youdao.com/yws/api/personal/file/WEBe97fe01bbaf7282c7ebffe1901a437b7?method=download&inline=true&shareKey=7a07ba40ceee734f609011099b8f72c5 "width=400 height=400")
 
 ### HTML引入粒子单页
 {{html:../danye/css/xingkong.html,width:100%,height:400px}}
@@ -117,6 +117,7 @@ console.log("Hello, World!");
 
 
 export default [note1, note2, note3, note4];
+
 
 
 
