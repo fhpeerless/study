55ba4f76ff45706a0624641162f417534b1895b1
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2026-01-16 12:24:47 +0800

    Add files via upload

--- notes/english/chapter1.js
+++ notes/english/chapter1.js
@@ -2,11 +2,19 @@
 // 第一篇笔记
 export const note1 = {
     title: "时态一般疑问句！",
-    content: [
-        "此笔记内容是上学时写的，总共有70多章，大约400多页，之后可能会出视频讲解，继续上传所有，如果学习上有问题请滑动到网页底部，加入QQ群聊共同探讨",
-        "语法分析，句子纠错，可能需要登录（国内可用）<br> <a href=\"https://wordvice.ai/cn/proofreading\">语法分析</a>， <br>或用百度出的ai查询语法正确与否<br>--><a href=\"https://www.baidu.com/index.htm\">ai语法查询(文心一言)c</a>   <br>更多查询google（国外可用）--><a href=\"https://www.google.com/\">google</a>",
+    contentmd:`
+## 英语简介
+"此笔记内容是上学时写的，总共有70多章，大约400多页，之后可能会出视频讲解，继续上传所有，如果学习上有问题请滑动到网页底部，加入QQ群聊共同探讨",
+        
+点击--> [语法分析](https://wordvice.ai/cn/proofreading "WebStorm") 相关语法
+ 
 
-    ],
+点击--> [ai查询语法正确](https://www.baidu.com/index.htm "WebStorm") ai语法查询(文心一言)
+
+ 点击--> [更多查询google（科学上网）](https://www.google.com "WebStorm") ai语法
+
+
+`,
     timestamp: "2025-10-10 08:30",
     images: [
         "http://note.youdao.com/yws/api/personal/file/WEB1f1b8b1a3ddcc3f3ef698a5f8bc7c9f6?method=download&inline=true&shareKey=969f3d4830d0aac90daf21837b4b185e"
@@ -19,28 +27,29 @@ export const note1 = {
 // 第一篇笔记
 export const note2 = {
     title: "时态及一般疑问句的句子结构！",
-    content: [
+    contentmd:`
         "此笔记内容是上学时写的，总共有70多章，大约400多页，之后可能会出视频讲解，继续上传所有，如果学习上有问题请滑动到网页底部，加入QQ群聊共同探讨",
         "语法分析，句子纠错，可能需要登录（国内可用）<br> <a href=\"https://wordvice.ai/cn/proofreading\">语法分析</a>， <br>或用百度出的ai查询语法正确与否<br>--><a href=\"https://www.baidu.com/index.htm\">ai语法查询(文心一言)c</a>   <br>更多查询google（国外可用）--><a href=\"https://www.google.com/\">google</a>",
 
-    ],
+`,
     timestamp: "2025-10-10 08:30",
     images: [  
         "http://note.youdao.com/yws/api/personal/file/WEB6a6c94676b91d0bd611fcd0a3628d05a?method=download&inline=true&shareKey=ad4f15b141d4a4776ad98b4526e38d4b",
         "http://note.youdao.com/yws/api/personal/file/WEBc3b7683be02a656e46b7f065a75dea55?method=download&inline=true&shareKey=eef201f3027b7b1b4c8cabd74d4c153e"
-        
     ],
     embed: ""
 };
+
 // HTML第一章 多篇笔记（改为命名导出+数组汇总）
 // 第一篇笔记
 export const note3 = {
     title: "一般疑问句的造句示例！",
-    content: [
+    contentmd:`
+
         "此笔记内容是上学时写的，总共有70多章，大约400多页，之后可能会出视频讲解，继续上传所有，如果学习上有问题请滑动到网页底部，加入QQ群聊共同探讨",
         "语法分析，句子纠错，可能需要登录（国内可用）<br> <a href=\"https://wordvice.ai/cn/proofreading\">语法分析</a>， <br>或用百度出的ai查询语法正确与否<br>--><a href=\"https://www.baidu.com/index.htm\">ai语法查询(文心一言)c</a>   <br>更多查询google（国外可用）--><a href=\"https://www.google.com/\">google</a>",
+`,
 
-    ],
     timestamp: "2025-10-10 08:30",
     images: [
        
