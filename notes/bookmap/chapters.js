3311f77681f110c8fca54bd0c754d82e6d5cada9
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2026-01-17 11:02:44 +0800

    Update chapters.js

--- notes/bookmap/chapters.js
+++ notes/bookmap/chapters.js
@@ -1,11 +1,6 @@
 // Python学科所有章节标题定义
 export default [
-    "图书推荐",
-    "H5jscss_学习路径",
-    "论坛开发全掌握",
-    "http由零入深",
-    "python由零入深",
-    "政治101研考学习方法",
-    "c语言系列学习路径",
-    "app开发学习路径"
+    "css/html学习推荐",
+    "javascript"
+
 ];
