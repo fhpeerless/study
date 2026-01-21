56b5fc8c2345ef116eeef5017a79c5fa962e70fe
fhpeerless <68208932@qq.com>
2026-01-21 11:34:00 +0800

    123

--- notes/css/chapter4.js
+++ notes/css/chapter4.js
@@ -1,6 +1,97 @@
 // 第一篇笔记
 export const note1 = {
-    title: "css初识",
+    title: "css的颜色梗概",
+    contentmd:` 
+# css的颜色概述----
+## 颜色名
+使用颜色名来定义颜色值，颜色不能调节深浅，颜色的属性是**color**： 值为颜色名或rgb，hex，hsl
+
+点击--> [css的颜色代码](https://www.w3school.com.cn/css/css_colors.asp "WebStorm") 了解css的颜色代码使用
+## 背景色----
+背景颜色的属性和值的代码 
+\`\`\`css copy
+<h1 style="background-color:DodgerBlue;">
+\`\`\` 
+
+点击--> [css的背景颜色演示](https://www.w3school.com.cn/tiy/t.asp?f=css_color_background "WebStorm") 了解css的背景颜色代码使用
+
+## 文本颜色----
+文本颜色是定义了包裹了文本的标签
+\`\`\`css copy
+<h1 style="color:Tomato;">China</h1>
+<p style="color:DodgerBlue;">China is a great country!</p>
+<p style="color:MediumSeaGreen;">China, officially the People's Republic of China...</p>
+\`\`\`
+
+点击--> [css的背景颜色演示](https://www.w3school.com.cn/tiy/t.asp?f=css_color_text "WebStorm") 了解css的背景颜色代码使用
+
+## 边框颜色----
+边框颜色是定义了包裹了文本的标签的颜色属性
+\`\`\`css copy
+<h1 style="color:Tomato;">China</h1>
+<p style="color:DodgerBlue;">China is a great country!</p>
+<p style="color:MediumSeaGreen;">China, officially the People's Republic of China...</p>
+\`\`\`
+
+点击--> [css的边框颜色演示](https://www.w3school.com.cn/tiy/t.asp?f=css_color_border "WebStorm") 了解css的背景颜色代码使用
+
+`,
+    timestamp: "2025-10-18 08:30",
+
+    embed: ""
+};
+
+// 第一篇笔记
+export const note2 = {
+    title: "css的rgb调色",
+    contentmd:` 
+# css的rgb调色使用
+
+
+点击--> [css的rgb](https://www.w3school.com.cn/css/css_colors_rgb.asp "WebStorm") 了解css的rgb调色
+
+rgb的使用方式
+\`\`\`css copy
+<h1 style="background-color:rgb(238, 130, 238);">rgb(238, 130, 238)</h1>
+\`\`\`
+
+# rgba的调色使用，是基于rgb加了一个透明度的属性！
+
+
+
+`,
+    timestamp: "2025-10-18 08:30",
+
+    embed: ""
+};
+
+
+// 第一篇笔记
+export const note3 = {
+    title: "css的润色",
+    contentmd:` 
+### css概述（了解一下即可）
+
+- 可以把html比作造型的过程，则css相当于给素描上色的过程，html编写内容，css布局等,js相当于动画编写
+ ### css的发展史
+ /* 在JSON中，字符串内部的双引号必须用反斜杠转义，即\"。以下是修正后的JSON： */
+<br>   
+点击--> [css的发展史](https://www.w3school.com.cn/tags/tag_map.asp "WebStorm") 了解css的发展史
+
+点击--> [css的发展史](https://www.webdesignmuseum.org/all-websites\ "WebStorm") 了解css的发展史
+
+点击--> [css发展之网页的变迁](https://www.webdesignmuseum.org/all-websites\ "WebStorm") 了解，不同时期的css代码进化后而随之改变的网页样式-->
+
+`,
+    timestamp: "2025-10-18 08:30",
+
+    embed: ""
+};
+
+
+// 第一篇笔记
+export const note4 = {
+    title: "css的润色",
     contentmd:` 
 ### css概述（了解一下即可）
 
@@ -23,9 +114,7 @@ export const note1 = {
 
 
 
-export default [note1];
-
-
+export default [note1, note2, note3, note4];
 
 
 
