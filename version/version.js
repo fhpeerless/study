59d783b358ec7bcd54cfb5812275e829da4a2a64
fhpeerless <68208932@qq.com>
2026-08-11 12:57:44 +0800

    版本号

---
+++ version/version.js
@@ -0,0 +1,10 @@
+// App版本信息
+// 版本号格式: v主版本.次版本.修订号.构建号 (如 v1.0.0.0)
+// 当本地版本落后远程版本3个构建号及以上时，App会强制要求更新
+export const version = {
+    version: "v1.0.0.0",
+    updateTime: "2026-08-11 11:10",
+    description: "初始版本：Flutter安卓App，支持学科分类浏览、笔记Markdown渲染、版本更新检查"
+};
+
+export default version;
