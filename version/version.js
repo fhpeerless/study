458d82b748504d1c74608ab1f10b4e80a9fce458
fhpeerless <68208932@qq.com>
2026-08-12 13:45:40 +0800

    书架更新

--- version/version.js
+++ version/version.js
@@ -27,7 +27,7 @@ export const versions = [
     {
         version: "v1.0.0.5",
         updateTime: "2026-08-12 00:05",
-        description: "新增笔记本地缓存和本地笔记页面",
+        description: "新增文字转语音，新增书架",
         updateUrl: "https://note.youdao.com/yws/api/personal/file/WEB8c3944019c4b4d5866cc0f133f710369?method=download&inline=true&shareKey=a4bdda357fa79cd28891df2f29cadc43",
         forceUpdate: false
     }
