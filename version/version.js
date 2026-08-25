a75e9b942989bd6b8853839d77d7c5d6e4dc1da6
fhpeerless <68208932@qq.com>
2026-08-25 17:22:05 +0800

    版本更新

--- version/version.js
+++ version/version.js
@@ -42,7 +42,7 @@ export const versions = [
         version: "v1.0.0.10",
         updateTime: "2026-08-25 08:16",
         description: "题库：登录激活、按省市浏览真题、下载/答题/删除/PDF，界面美化，跳题功能，新增本地PDF扫描",
-        updateUrl: "https://pan.ddda.cc/f/EKS7/study_1.0.0.11.apk",
+        updateUrl: "https://pan.ddda.cc/f/2bh9/study_1.0.0.10.apk",
         forceUpdate: true
     }
 ];
